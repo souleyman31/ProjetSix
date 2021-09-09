86eb16d788c8b4b51eb8eebaa277f2dfb83c8e61
Souleyman <soulmandra@gmail.com>
2021-09-09 09:57:33 +0200

    Protect the sauce's User

---
+++ middleware/basicAuth.js
@@ -0,0 +1,12 @@
+const Sauce = require("../models/Sauce");
+const { sauce } = require("./validate-input");
+
+module.exports = (req, res, next) => {
+  //   console.log(req.userFromToken);
+  Sauce.findOne({ _id: req.params.id }).then(sauce => {
+    if (sauce.userId !== req.userFromToken) {
+      return res.status(403).json({ msg: "Vous n'etes pas le proprietaire" });
+    }
+    next();
+  });
+};
